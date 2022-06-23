5abd14bce2d199e9b05978f7014cb43e1bbf5bdb
Ильяс <106968484+Fibby1@users.noreply.github.com>
2022-06-23 10:51:59 +0300

    абырвал2

--- app/src/main/java/com/example/geoquiz/QuizViewModel.kt
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -5,20 +5,27 @@ import androidx.lifecycle.ViewModel
 
 private const val TAG = "QuizViewModel"
 
-class QuizViewModel : ViewModel(){
+class QuizViewModel : ViewModel() {
 
     var currentIndex = 0
+    var isCheater = false
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
         Question(R.string.question_oceans, true),
-        Question(R.string.question_mediast, true),
-        Question(R.string.question_africa, true),
+        Question(R.string.question_mediast, false),
+        Question(R.string.question_africa, false),
         Question(R.string.question_americas, true),
         Question(R.string.question_asia, true)
     )
-    val currentQuestionAnswer: Boolean get() = questionBank[currentIndex].answer
-    val currentQuestionText:Int get() = questionBank[currentIndex].textResId
-    fun moveToNext(){currentIndex = (currentIndex +1 ) % questionBank.size
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
     }
 }
